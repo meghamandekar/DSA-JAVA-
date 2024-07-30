5bc96de0dd78e791962546b6975d47c7f48d70dc
Megha Mandekar <86413849+meghamandekar@users.noreply.github.com>
2024-07-30 23:02:41 +0530

    Add files via upload

---
+++ balancedstringsplit.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+    int count=0;
+    int r=0;
+    int l=0;
+    
+    for(int i=0;i<s.length();i++){
+        if(s.charAt(i)=='R')
+            r++;
+        else
+            l++;
+        if(r==l)
+        {
+            count++;
+            r=0;
+            l=0;
+        }
+    }
+    return count;
+    }
+}
